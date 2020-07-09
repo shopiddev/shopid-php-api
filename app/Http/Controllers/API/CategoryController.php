986bd8dcd2613aaad0269d488da963dc98763601
shopiddev <alaeebehnam@gmail.com>
2020-07-09 14:58:11 +0430

    create category model and controller

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Category;
+
+class CategoryController extends Controller
+{
+    function addnew(Request $request) {
+		
+		
+		      
+		
+		        $cat = Category::create([
+                'title' => $request['title'], 'parent' => (isset($request['parent']) ? $request['parent'] : 0) , ]);
+				
+				return $cat;
+				
+	}
+	
+	
+
+}
